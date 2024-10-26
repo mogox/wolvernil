9f9ded042239a87be33dd0a236869be8d93392fa
Enrique Carlos Mogollan <emogollan@salesforce.com>
2024-10-26 15:12:31 -0700

    initial commit

---
+++ lib/wolvernil.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wolvernil/version"
+
+module Wolvernil
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wolvernil/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Wolvernil
+  VERSION = "0.1.0"
+end
