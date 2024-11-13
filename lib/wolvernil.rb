5f56b3b89176e6ff391be6d26dcada0baf984948
Enrique Carlos Mogollan <emogollan@salesforce.com>
2024-11-13 08:55:08 -0800

    Let's do this!

--- lib/wolvernil.rb
+++ lib/wolvernil.rb
@@ -1,8 +1,76 @@
 # frozen_string_literal: true
 
 require_relative "wolvernil/version"
+require_relative "wolvernil/nil_tracker"
 
 module Wolvernil
+  def method_missing(method, *args, &block)
+    return if method == :call
+
+    nil_tracker.log(method, caller.take(5).join("\n"), args, block)
+
+    if raise_exception?
+      error_message = "=====> Trying to call method `#{method}` nil (NoMethodError)"
+      puts "====> Raising NoMethodError <=====\n\n"
+      raise NoMethodError.new(error_message)
+    else
+      puts "=====> NoMethodError not raised for method #{method}"
+      puts "================================\n\n"
+    end
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    false
+  end
+
+    def raise_exception?
+    nil_tracker.raise_exception
+  end
+
+  def raise_exception(value)
+    nil_tracker.raise_exception = value
+  end
+
+  def self.methods_list
+    NilTracker.instance.methods_list
+  end
+
+  def to_ary
+    []
+  end
+
+  def to_h
+    {}
+  end
+
+  def to_hash
+    {}
+  end
+
+  def to_str
+    ""
+  end
+
+  def []=(key, value)
+  end
+
+  def process_wait
+    super
+  end
+
+  def call(*args)
+    super(args)
+  end
+
+  def permitted?
+    false
+  end
+
+  private
+
+  def nil_tracker
+    Wolvernil::NilTracker.instance
+  end
+
   class Error < StandardError; end
-  # Your code goes here...
 end
---
+++ lib/wolvernil/nil_tracker.rb
@@ -0,0 +1,41 @@
+require 'singleton'
+
+module Wolvernil
+  class NilTracker
+    include Singleton
+
+    attr_accessor :raise_exception
+
+    def methods_list
+      @methods_list ||= {}
+    end
+
+    def log(method, caller_lines, *args,  &block)
+      error_message = "=====> Trying to call method **#{method}** in `nil` (NoMethodError)"
+
+      puts error_message
+      puts "=====> Caller: (stacktrace)"
+      puts caller.take(5).join("\n")
+      puts "=====> args: #{args}" if args&.size > 0
+      puts "=====> block: #{block.source}" if block
+
+      create_method(method, args, block)
+      store_method_info(method, caller_lines, args, block)
+    end
+
+    def create_method(method, *args, &block)
+      return if methods_list[method]
+
+      Wolvernil.define_method(method) do |*args, &block|
+        p "---> Calling a method #{method} in the nil class, this has been logged"
+      end
+    end
+
+    def store_method_info(method, caller_lines, *args,  &block)
+      new_block = args[1].source if args[1]&.class == Proc
+      methods_list[method] =  {
+        args:, block: new_block, caller_lines:, timestamp: Time.now
+      }
+    end
+  end
+end
